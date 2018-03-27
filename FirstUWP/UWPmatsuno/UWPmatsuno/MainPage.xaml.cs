314fa1290bbb737f65a85c6ae84dfd41b5188b5f
kawaueshingo <singu222@yahoo.co.jp>
2018-03-28 00:29:24 +0900

    edit calc

--- FirstUWP/UWPmatsuno/UWPmatsuno/MainPage.xaml.cs
+++ FirstUWP/UWPmatsuno/UWPmatsuno/MainPage.xaml.cs
@@ -13,6 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
+using UWPmatsuno.ToLibrary; //自作クラス
+
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
 namespace UWPmatsuno
@@ -22,44 +25,115 @@ namespace UWPmatsuno
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool bIsCalculated = false;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        /// ////////////////////////////////////////////////イベントハンドラ//////////////////////////////////////////////////////////////////
+
+
         private void Button_Calc_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock_Message.Text = TextBox_StartingTime.Text + TextBox_LeavingTime.Text;
-<<<<<<< HEAD
-=======
-            //DateTimeに変換できるか確かめる
-            DateTime dt, dt2;
+            CalcDailyWage();
+        }
 
-            if ( (DateTime.TryParseExact(TextBox_StartingTime.Text, "H:m" , System.Globalization.CultureInfo.InvariantCulture ,
-                System.Globalization.DateTimeStyles.None , out dt)) &&
-                (DateTime.TryParseExact(TextBox_LeavingTime.Text, "H:m", System.Globalization.CultureInfo.InvariantCulture,
-                System.Globalization.DateTimeStyles.None, out dt2))   )
+        private void ToSw_Kyuukei_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (bIsCalculated == true) CalcDailyWage();
+        }
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        private void CalcDailyWage()//日給計算
+        {
+            TextBlock_Message.Text = TextBox_Shukkin.Text + TextBox_Taisha.Text;
+
+            //DateTimeに変換できるか確かめる
+            DateTime dt ,dt2;
+            if ( !(
+                DateTime.TryParseExact(TextBox_Shukkin.Text, "H:m", System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out dt)
+                ) 
+                &&
+                !(
+                DateTime.TryParseExact(TextBox_Shukkin.Text, "HHmm", System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out dt)
+                ))
             {
-                TimeSpan ts;
-                //変換出来たら、dtにその値が入る
+                EnterCorrectTime(); return;//変換できなかったら
+            }
+
+
+            if ( !(
+                DateTime.TryParseExact(TextBox_Taisha.Text, "H:m", System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out dt2)
+                ) 
+                &&
+                 !(
+                DateTime.TryParseExact(TextBox_Taisha.Text, "HHmm", System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out dt2)
+                )
+                )
+            {
+                EnterCorrectTime(); return;//変換できなかったら
+            }
+
+
+            //変換出来たら、dt,dt2に値が入っている
+
+            TimeSpan ts;
+                
                 if (dt2.CompareTo(dt) > 0)
                 {
-                    
+
                     ts = (dt2 - dt);
                 }
                 else
                 {
-                    ts = ( dt - dt2);
+                    ts = (dt - dt2);//日またぎ
                     ts = TimeSpan.FromHours(24) - ts;
                 }
 
-                TextBlock_Message.Text = ts.ToString();
-            }
-            else
-            {//変換できなかったら
-                TextBlock_Message.Text = "適切な時刻を入力してください";
-            }
->>>>>>> add timespancalc
+                TextBlock_ResultTS.Text = "時間 : " + ts.Hours.ToString().PadLeft(2, '0') + ":" + ts.Minutes.ToString().PadLeft(2, '0');
+                TextBlock_Message.Text = "出勤、退社時間";
+                //給料の計算
+
+                int kyuukei = 0;
+                if (ToSw_Kyuukei.IsOn)//休憩時間を引くトグルスイッチがオンなら
+                {
+                    kyuukei = CalcWorkingTime.SubBreakTime(ref ts);
+                    TextBlock_ResultTS.Text = TextBlock_ResultTS.Text + @"
+休憩時間:" + kyuukei + "分";
+                }
+
+                TextBlock_ResultMoney.Text = " 給料:\\" +
+                    String.Format("{0:#,0}", (ts.Hours * 1000 + ts.Minutes * 1000 / 60)) +
+                    ".-";
+
+                bIsCalculated = true;
+            
+
+
         }
+
+
+
+        private void EnterCorrectTime()
+        {
+            TextBlock_Message.Text = "適切な時刻を入力してください";
+            TextBlock_ResultMoney.Text = "";
+            TextBlock_ResultTS.Text = "";
+        }
+
+
+
+
+
+
+
+
     }
 }
---
+++ FirstUWP/UWPmatsuno/UWPmatsuno/ToLibrary/CalcWorkingTime.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPmatsuno.ToLibrary
+{
+    public static class CalcWorkingTime
+    {
+
+        //int型の値の先頭に0を付けたstring型を返す
+        //戻り値　引いた分数
+        public static int SubBreakTime(ref TimeSpan ts)
+        {
+            if (ts.TotalMinutes < 0)
+            {
+                throw new Exception("SubBreakTimeの引数が無効な値です");
+            }
+
+            TimeSpan ts2 = new TimeSpan(5, 0, 0);
+            if (ts < ts2) return 0;//5時間未のとき：引かない
+
+
+           ts2 = new TimeSpan(5, 30, 0);
+            if (ts < ts2)//5時間以上５時間半未満のとき：５時間分を超えた分ひく
+            {
+                int minOver = ts.Minutes;
+                ts = new TimeSpan(5, 0, 0);
+                return minOver;
+            }
+
+            ts2 = new TimeSpan(10, 00, 0);
+            if (ts < ts2)//５時間半以上１０時間未満のとき：３０分ひく
+            {
+                double totalmin;
+                int hours, min;
+                totalmin = ts.TotalMinutes - 30;//30分ひく
+                hours = (int)totalmin / 60;
+                min = (int)totalmin % 60;
+
+                ts = new TimeSpan(hours, min , 0);
+                return 30;
+            }
+
+            ts2 = new TimeSpan(10, 30, 0);
+            if (ts < ts2)//10時間以上１０時間30分未満のとき：３０分+１０時間を超えた分だけひく
+            {
+                double totalmin;
+                int hours, min , minOver;
+                minOver = ts.Minutes;
+                totalmin = ts.TotalMinutes - 30 - minOver;//30分と１０時間を超えた分ひく
+                hours = (int)totalmin / 60;
+                min = (int)totalmin % 60;
+
+                ts = new TimeSpan(hours , min, 0);
+                return 30 + minOver;
+            }
+
+
+//１０時間半以上のとき：１時間ひく
+ts = new TimeSpan(ts.Hours - 1, ts.Minutes, 0);
+            return 60;
+        }
+
+    }
+
+
+}
